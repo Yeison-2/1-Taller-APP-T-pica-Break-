b5e58e84468ffe6296a552b7bd270f4b70a83698
Yeison-2 <yeisonstivenromero@gmail.com>
2026-09-05 23:12:23 -0500

    Solucion de emulador apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.zyrdev.calculadorabreak"
-        minSdk = 26
+        minSdk = 24
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
